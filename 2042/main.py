3d2269de422886e3beef4ca316d122df296cdcac
YangTaeyoung <0130yang@gmail.com>
2022-05-23 23:19:07 +0900

    (BOJ-2042) 왜 안될까

--- 2042/main.py
+++ 2042/main.py
@@ -2,13 +2,12 @@
 
 import sys
 
-N, M = map(int, sys.stdin.readline().split())
-nodes = [int(input()) for _ in range(N)]
-trees = [0] * (2 * N)
+N, M, K = map(int, sys.stdin.readline().split())
+nodes = [int(sys.stdin.readline()) for _ in range(N)]
+trees = [0] * (4 * N)
 
 
 def init(start, end, ti):
-    print(f"{start} {end} {ti}")
     if start == end:
         trees[ti] = nodes[start - 1]
     else:
@@ -18,16 +17,28 @@ def init(start, end, ti):
 
 
 def segment_sum(start, end, left, right, ti):
-    if start == left and end == right:
-        return trees[ti]
-    elif start <= left <= end and start <= right <= end:
-        mid = (start + end) // 2
-        return segment_sum(start, mid, left, right, ti * 2) + segment_sum(mid+1, end, left, right, ti * 2)
-    #elif :
-
-    else:
+    if end < left or start > right:  # 구하려는 범위 밖에 노드 범위가 위치
         return 0
+    if start >= left and right >= end:  # 구하려는 범위 안에 노드 범위가 위치
+        return trees[ti]
+    # 범위에 겹쳐있음
+    mid = (start + end) // 2
+    return segment_sum(start, mid, left, right, ti * 2) + segment_sum(mid + 1, end, left, right, ti * 2 + 1)
+
+
+def update_node(start, end, node, ti, dif):
+    if start <= node <= end:
+        trees[ti] += dif
+        if start != end:  # 리프노드
+            mid = (start + end) // 2
+            update_node(start, mid, node, ti * 2, dif)
+            update_node(mid + 1, end, node, ti * 2 + 1, dif)
 
 
 init(1, N, 1)
-print(trees)
+for _ in range(M + K):
+    a, b, c = map(int, sys.stdin.readline().split())
+    if a == 1:  # 수정
+        update_node(1, N, b, 1, c - nodes[b-1])
+    else:  # 구간 합
+        print(segment_sum(1, N, b, c, 1))
