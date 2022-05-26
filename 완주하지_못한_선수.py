69e205748b1e05f4b0686932457eeabbcbc63288
MKsson <wnsldj2264@gmail.com>
2022-05-26 20:26:09 +0900

    Solved: 완주하지 못한 선수.

---
+++ 완주하지_못한_선수.py
@@ -0,0 +1,24 @@
+# 프로그래머스 - 완주하지 못한 선수: https://programmers.co.kr/learn/courses/30/lessons/42576
+
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    for i in range(len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+    return participant[-1]
+
+
+# 효율 안나온 풀이
+# def solution(participant, completion):
+#     for i in completion:
+#         if i in participant:
+#             participant.remove(i)
+#     answer = participant[0]
+#     return answer
+
+p = ["mislav", "stanko", "mislav", "ana"]
+c = ["stanko", "ana", "mislav"]
+
+result = solution(p, c)
+print(result)
