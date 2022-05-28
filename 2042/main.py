c2175e81156dad8327f6d4d3570fb8711f865379
Taeyoung Yang <59782504+YangTaeyoung@users.noreply.github.com>
2022-05-29 01:01:38 +0900

    [양태영] (BOJ - 2042) 구간 합 구하기 풀이 (#12)
    * Docs: gitignore파일 idea 제외 설정 추가
    
    * (BOJ - 2042) 구간 합 합치기 문제 푸는 중
    
    * (BOJ-2042) 왜 안될까
    
    * Solved: (BOJ-2042) 구간 합 구하기 풀이
    
    Co-authored-by: graffe_yang <graffewithcode@2digit.io>

---
+++ 2042/main.py
@@ -0,0 +1,45 @@
+# [BOJ - 2042] 구간 합 구하기 - https://www.acmicpc.net/problem/2042
+
+import sys
+
+N, M, K = map(int, sys.stdin.readline().split())
+nodes = [int(sys.stdin.readline()) for _ in range(N)]
+trees = [0] * (4 * N)
+
+
+def init(start, end, ti):
+    if start == end:
+        trees[ti] = nodes[start]
+    else:
+        mid = (start + end) // 2
+        trees[ti] = init(start, mid, ti * 2) + init(mid + 1, end, ti * 2 + 1)
+    return trees[ti]
+
+
+def segment_sum(start, end, left, right, ti):
+    if end < left or start > right:  # 구하려는 범위 밖에 노드 범위가 위치
+        return 0
+    if start >= left and right >= end:  # 구하려는 범위 안에 노드 범위가 위치
+        return trees[ti]
+    # 범위에 겹쳐있음
+    mid = (start + end) // 2
+    return segment_sum(start, mid, left, right, ti * 2) + segment_sum(mid + 1, end, left, right, ti * 2 + 1)
+
+
+def update_node(start, end, node, ti, dif):
+    if start <= node <= end:
+        trees[ti] += dif
+        if start != end:  # 리프노드가 아닌 경우
+            mid = (start + end) // 2
+            update_node(start, mid, node, ti * 2, dif)
+            update_node(mid + 1, end, node, ti * 2 + 1, dif)
+
+
+init(0, N - 1, 1)
+for _ in range(M + K):
+    a, b, c = map(int, sys.stdin.readline().split())
+    if a == 1:  # 수정
+        update_node(0, N - 1, b - 1, 1, c - nodes[b - 1])
+        nodes[b - 1] = c
+    else:  # 구간 합
+        print(segment_sum(0, N - 1, b - 1, c - 1, 1))
