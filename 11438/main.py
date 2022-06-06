cad760e7cb06248383262a5e39b422a93e0a2799
YangTaeyoung <0130yang@gmail.com>
2022-06-07 02:41:04 +0900

    Solved: (BOJ-11438) 최소 공통 조상 풀이

---
+++ 11438/main.py
@@ -0,0 +1,60 @@
+import sys
+
+
+def dfs(node: int, d: int):
+    # 깊이 설정 (현재노드)
+    depth[node] = d
+    visited[node] = True
+    # 자식 노드의 부모 설정
+    for adj_node in graph[node]:
+        if not visited[adj_node]:
+            parent[adj_node][0] = node
+            dfs(adj_node, d + 1)
+
+
+def set_parent():
+    dfs(1, 0)
+    for i in range(1, LOG):
+        for j in range(1, N + 1):
+            parent[j][i] = parent[parent[j][i - 1]][i - 1]
+
+
+def lca(node_1, node_2):
+    # 노드의 깊이는 1번이 더 작게 설정
+    if depth[node_1] > depth[node_2]:
+        node_1, node_2 = node_2, node_1
+
+    # 두 노드의 depth 맞추기
+    for i in range(LOG - 1, -1, -1):
+        if depth[node_2] - depth[node_1] >= 2 ** i:
+            node_2 = parent[node_2][i]
+
+    if node_1 == node_2:
+        return node_1
+
+    for i in range(LOG - 1, -1, -1):
+        if parent[node_1][i] != parent[node_2][i]:
+            node_1 = parent[node_1][i]
+            node_2 = parent[node_2][i]
+
+    return parent[node_1][0]
+
+
+LOG = 21
+N = int(sys.stdin.readline())
+depth = [0] * (N + 1)
+parent = [[0] * LOG for _ in range(N + 1)]
+visited = [False] * (N + 1)
+graph = [[] for _ in range(N + 1)]
+
+for _ in range(N - 1):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+set_parent()
+
+M = int(sys.stdin.readline())
+for _ in range(M):
+    a, b = map(int, sys.stdin.readline().split())
+    print(lca(a, b))
--- 11438/try_1.py
+++ 11438/try_1.py
@@ -1,63 +1,53 @@
-# import sys
-#
-#
-# class Node:
-#     def __init__(self, data, depth, parent=None):
-#         self.
-#         data = data
-#         self.parent = parent
-#         self.depth = depth
-#
-#
-# class Tree:
-#     def __init__(self):
-#         self.root = Node(1, 1)
-#         self.nodes = [self.root]
-#
-#     def find_node(self, data):
-#         for n_i in range(len(self.nodes)):
-#             if self.nodes[n_i].data == data:
-#                 return n_i
-#         return -1
-#
-#     @staticmethod
-#     def find_depth_parent(node, depth):
-#         tmp_node = node
-#         while tmp_node.depth != depth:
-#             tmp_node = tmp_node.parent
-#         return tmp_node
-#
-#     def insert_node(self, parent, data):
-#         if self.find_node(parent) == -1:
-#             parent, data = data, parent
-#         parent = self.nodes[self.find_node(parent)]
-#         new_node = Node(data, parent.depth + 1, parent)
-#         self.nodes.append(new_node)
-#
-#     def get_lca(self, node1_data, node2_data):
-#         node1 = self.nodes[self.find_node(node1_data)]
-#         node2 = self.nodes[self.find_node(node2_data)]
-#         max_depth_node = max(node1, node2, key=lambda x: x.depth)
-#         min_depth_node = min(node2, node1, key=lambda x: x.depth)
-#
-#         max_depth_node = self.find_depth_parent(max_depth_node, min_depth_node.depth)
-#
-#         while max_depth_node.data != min_depth_node.data:
-#             max_depth_node = max_depth_node.parent
-#             min_depth_node = min_depth_node.parent
-#         return max_depth_node.data
-#
-#
-# tree = Tree()
-# N = int(sys.stdin.readline())
-# for _ in range(N - 1):
-#     a, b = map(int, sys.stdin.readline().split())
-#     tree.insert_node(a, b)
-#
-# M = int(sys.stdin.readline())
-# for _ in range(M):
-#     a, b = map(int, sys.stdin.readline().split())
-#     print(tree.get_lca(a, b))
-# 시간 초과..
+import sys
 
-#
+
+# 시간 초과
+class Node:
+    def __init__(self, data, depth, parent=None):
+        self.data = data
+        self.parent = parent
+        self.depth = depth
+
+
+class Tree:
+    def __init__(self):
+        self.root = Node(1, 1)
+        self.nodes = {1: self.root}
+
+    @staticmethod
+    def find_depth_parent(node, depth):
+        tmp_node = node
+        while tmp_node.depth != depth:
+            tmp_node = tmp_node.parent
+        return tmp_node
+
+    def insert_node(self, parent, data):
+        if not self.nodes.get(parent):
+            parent, data = data, parent
+        parent = self.nodes.get(parent)
+        self.nodes[data] = Node(data, parent.depth + 1, parent)
+
+    def get_lca(self, node1_data, node2_data):
+        node1 = self.nodes.get(node1_data)
+        node2 = self.nodes.get(node2_data)
+        max_depth_node = max(node1, node2, key=lambda x: x.depth)
+        min_depth_node = min(node2, node1, key=lambda x: x.depth)
+
+        max_depth_node = self.find_depth_parent(max_depth_node, min_depth_node.depth)
+
+        while max_depth_node.data != min_depth_node.data:
+            max_depth_node = max_depth_node.parent
+            min_depth_node = min_depth_node.parent
+        return max_depth_node.data
+
+
+tree = Tree()
+N = int(sys.stdin.readline())
+for _ in range(N - 1):
+    a, b = map(int, sys.stdin.readline().split())
+    tree.insert_node(a, b)
+
+M = int(sys.stdin.readline())
+for _ in range(M):
+    a, b = map(int, sys.stdin.readline().split())
+    print(tree.get_lca(a, b))
