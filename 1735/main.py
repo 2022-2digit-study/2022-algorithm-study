8d346c3cbbd6428940cb6cdff6eeaf4897d70323
YangTaeyoung <0130yang@gmail.com>
2022-06-22 17:26:00 +0900

    Enhance: 파이썬 math 모듈 사용

--- 1735/main.py
+++ 1735/main.py
@@ -1,13 +1,6 @@
 # (BOJ -1735) 분수 합: https://www.acmicpc.net/problem/1735
 import sys
-
-
-def get_gcd(a, b):
-    for i in range(min(a, b), 0, -1):
-        if not a % i and not b % i:
-            return i
-    return 1
-
+import math
 
 nums = []
 for _ in range(2):
@@ -17,5 +10,5 @@ for _ in range(2):
 calc_boon_mom = nums[0][1] * nums[1][1]
 calc_boon_son = nums[0][0] * nums[1][1] + nums[1][0] * nums[0][1]
 
-gcd = get_gcd(calc_boon_mom, calc_boon_son)
+gcd = math.gcd(calc_boon_mom, calc_boon_son)
 print(calc_boon_son // gcd, calc_boon_mom // gcd)
